2d1bd67aa343c7395552c7df1dcdc3ab2e17f242
Edgett <030617@corpaa.aa.com>
2017-09-18 08:27:21 -0500

    i should probably put a description here

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,24 +1,63 @@
-/*
- * Johnny Edgett
+/**
+ * @author Johnny Edgett
+ * @version 1.01
  * This is the client side of a small client-server application. 
  * When the user beeps, the server boops!
  * Also it uses threads because I am trying to brush up on my programming...
  */
 package Client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Client {
     
-    public static void main(String[] args){
-        if(args.length != 3){
-            System.out.println("Usage: beep [server socket] [beepcount] [# threads]");
-            System.exit(-1);
-        }
+    /**
+     * 
+     */
+    public static void main(String[] args) throws IOException{
+        // End if there are not enough arguments
+        //if(args.length != 3){
+          //  System.out.println("Usage: beep [server socket] [beepcount] [# threads]");
+            //System.exit(-1);
+        //}
+        BufferedReader in = null;
+        PrintWriter out = null;
         
-        for(int i = 1; i <= args.length; i++){
-            new BeepThread(Integer.parseInt(args[0]), Integer.parseInt(args[1]), i).start();
+        // Test Block
+        int num1 = 1234;
+        int num2 = 2;
+        int num3 = 3;
+        // Test Block
+        
+        // Define the hostName here
+        String hostName= "localhost";
+        //int portNum = Integer.parseInt(args[0]);
+        
+        // Open up the socket to begin client-server communication
+        try{
+            Socket socket = new Socket(hostName, num1);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        out.println("beep");
+        
+        // Create BeepThreads
+        //for(int i = 1; i <= args.length; i++){
+        //    new BeepThread(num1, num2, 1).start();
+        //}
+        String response = null;
+        
+        while((response = in.readLine()) != null){
+            System.out.println(response);
         }
     }
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @author Johnny Edgett
+ * @version 1.01
+ * This is the server side of a small client-server application. 
+ * When the user beeps, the server boops!
+ * Also it uses threads because I am trying to brush up on my programming...
  */
 package Server;
 
-/**
- *
- * @author 030617
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Server {
-    
+    public static void main(String[] args){
+        int portNum = Integer.parseInt("1234");
+        PrintWriter out = null;
+        BufferedReader in = null;
+        
+        
+        try {
+            ServerSocket socket = new ServerSocket(portNum);
+            Socket clientSocket = socket.accept(); // Wait for an incoming connection
+            
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } 
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        String clientInput = null;
+        String serverOutput = null;
+        
+        try {
+            clientInput = in.readLine();
+            System.out.println(clientInput);
+            if(clientInput.equals("beep"))
+            {
+                out.println("boop");
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        System.out.println(clientInput);
+    }
 }
